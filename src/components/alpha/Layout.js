c5d7c218f4be80ea0b38d095b3031f997e93cb89
Chad Fernandez <chadfernandez@duck.com>
2022-09-16 13:14:26 +0800

    refactor to follow naming guideline

--- src/components/alpha/Layout.js
+++ src/components/alpha/Layout.js
@@ -1,15 +1,12 @@
 import Navigation from "./Navigation";
 
-function Layout({ children }) {
+export default function Layout({ children }) {
   return (
     <main className="subpixel-antialiased">
       <div className="sticky top-0 z-[100]">
         <Navigation />
       </div>
-      {/* usable height after navigation bar is 91.45vh */}
       <div>{children}</div>
     </main>
   );
 }
-
-export default Layout;
