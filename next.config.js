11721dec45d1be67bac0ee8ae583721bd08adc5a
Chad Fernandez <chadfernandez@duck.com>
2022-09-16 13:14:46 +0800

    update images source to include hosted in notion and from unsplash

--- next.config.js
+++ next.config.js
@@ -6,8 +6,11 @@ const nextConfig = {
   images: {
     domains: [
       "emojipedia-us.s3.dualstack.us-west-1.amazonaws.com",
-    ]
-  }
-}
+      "www.notion.so",
+      "s3.us-west-2.amazonaws.com",
+      "images.unsplash.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
