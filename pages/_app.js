6e2f72088d323db3d9c386c3623711910731edb0
Chad Fernandez <chadfernandez@duck.com>
2022-11-20 10:30:54 +0800

    moved `styles` folder to `/src`

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import "../styles/globals.css";
+import "/src/styles/globals.css";
 
 export default function app({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
