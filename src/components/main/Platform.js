c2e74ab60cabf59c11c88b42ed4bca6ea7e9a8b3
Chad Fernandez <chadfernandez@duck.com>
2022-11-22 19:37:43 +0800

    updated platform canvas size for mobile

--- src/components/main/Platform.js
+++ src/components/main/Platform.js
@@ -5,7 +5,7 @@ import { classMerge } from "../../utils/TailwindUtilities";
 export default function Platform({ children, className = "" }) {
   return (
     <div className={classMerge("flex flex-col items-center", className)}>
-      <div className="w-[90vw] md:w-[75vw] lg:w-[60vw]">{children}</div>
+      <div className="w-[95vw] md:w-[75vw] lg:w-[60vw]">{children}</div>
     </div>
   );
 }
