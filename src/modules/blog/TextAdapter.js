b6ff04560c9338a0a21d4402122b1ecd96c4bbbc
Chad Fernandez <chadfernandez@duck.com>
2022-11-24 12:01:29 +0800

    update blog post styling, and typography

--- src/modules/blog/TextAdapter.js
+++ src/modules/blog/TextAdapter.js
@@ -19,16 +19,22 @@ export const RichTextRender = ({ richText }) => {
         className={classMerge(
           bold ? "font-bold" : null,
           italic ? "italic" : null,
-          strikethrough ? "line-through" : null,
-          underline ? "underline" : null,
-          code ? "rounded bg-red-100 px-0.5 font-mono" : null,
+          strikethrough ? "line-through decoration-[0.5px]" : null,
+          underline ? "underline underline-offset-[3px]" : null,
+          code
+            ? "rounded bg-blue-100 px-0.5 align-middle font-mono text-sm font-semibold text-blue-700"
+            : null,
           colorMapper(color)
         )}
       >
+        {/* 
+        // Resolve if it has link or not. 
+        // TODO: Add check if internal or external link.
+        */}
         {text.link ? (
           <a href={text.link.url}>
             {text.content}
-            <ExternalLinkIcon className="ml-0.5 inline-flex h-3 w-3 items-center justify-center text-cyan-700" />
+            <ExternalLinkIcon className="ml-0.5 inline-flex h-3 w-3 items-center justify-center text-blue-700" />
           </a>
         ) : (
           text.content
@@ -57,21 +63,30 @@ export const blockRenderer = (block) => {
     case "heading_1":
       // TODO: Add support for toggleable heading 1.
       return (
-        <h1 className="mt-6 mb-2 text-2xl font-bold md:text-3xl" id={id}>
+        <h1
+          className="mt-6 mb-2 text-2xl font-bold text-black md:text-3xl"
+          id={id}
+        >
           <RichTextRender richText={value.rich_text} />
         </h1>
       );
     case "heading_2":
       // TODO: Add support for toggleable heading 2.
       return (
-        <h2 className="mt-6 mb-2 text-xl font-bold md:text-2xl" id={id}>
+        <h2
+          className="mt-6 mb-2 text-xl font-bold text-black md:text-2xl"
+          id={id}
+        >
           <RichTextRender richText={value.rich_text} />
         </h2>
       );
     case "heading_3":
       // TODO: Add support for toggleable heading 3.
       return (
-        <h3 className="mt-6 mb-2 text-lg font-bold md:text-xl" id={id}>
+        <h3
+          className="mt-6 mb-2 text-lg font-bold text-black md:text-xl"
+          id={id}
+        >
           <RichTextRender richText={value.rich_text} />
         </h3>
       );
@@ -86,28 +101,9 @@ export const blockRenderer = (block) => {
           <div className="flex max-h-[28rem] min-h-min w-full flex-col items-center justify-center overflow-clip rounded-lg">
             <img src={src} alt={caption} className="w-full aspect-auto" />
           </div>
-          <p className="text-sm text-gray-500">{caption}</p>
+          <p className="text-sm text-neutral-500">{caption}</p>
         </div>
       );
-
-    // // render image as full width but keep aspect ratio
-    // return (
-    //   <div className="mt-6 mb-6">
-    //     <div className="relative aspect-video w-full rounded-lg md:aspect-[16/9]">
-    //       <Image
-    //         className="rounded-lg"
-    //         src={src}
-    //         alt={caption}
-    //         layout="fill"
-    //         objectFit="cover"
-    //         priority
-    //       />
-    //     </div>
-    //     {caption ? (
-    //       <p className="mt-1 text-sm text-gray-400">{caption}</p>
-    //     ) : null}
-    //   </div>
-    // );
     case "bulleted_list_item":
     case "numbered_list_item":
       return (
