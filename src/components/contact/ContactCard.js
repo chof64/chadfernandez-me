2968c19e1aea19437e16f9e27ebf345ed790358b
Chad Fernanadez <contact@chadfernandez.me>
2022-11-30 00:28:45 +0000

    update contact card design and layout

--- src/components/contact/ContactCard.js
+++ src/components/contact/ContactCard.js
@@ -16,19 +16,22 @@ export default function ContactCard() {
           <h1 className="font-semibold text-neutral-500">Contact</h1>
         </div>
         <div className="w-full max-w-md rounded-md bg-gradient-to-r from-blue-600 to-cyan-400 p-0.5 shadow-xl shadow-gray-200">
-          <div className="rounded bg-gradient-to-r from-white via-white to-white/80 bg-repeat p-2 px-4 backdrop-blur-md">
-            <h1 className="text-xl font-bold">Chad Fernandez</h1>
-            <p className="text-sm text-neutral-700">(Fullstack Developer)</p>
-            <p className="mt-8 text-neutral-700">
+          <div className="relative rounded bg-gradient-to-r from-white via-white to-white/80 bg-repeat p-2 px-4 backdrop-blur-md">
+            <h1 className="text-2xl font-bold">Chad Fernandez</h1>
+            <p className="text-sm font-light text-neutral-700">
+              (Fullstack Developer)
+            </p>
+            <div className="mt-16">
+              <p className="align-middle text-neutral-700">
+                {/* <MailIcon className="inline-block h-4 w-4 stroke-neutral-500" />{" "} */}
+                <a href="mailto:contact@chadfernandez.me">
+                  contact@chadfernandez.me
+                </a>
+              </p>
+            </div>
+            <p className="absolute bottom-0 right-2 text-xs text-neutral-700">
               <a href="http://chadfernandez.me">chadfernandez.me</a>
             </p>
-            <p className="align-middle text-neutral-700">
-              {/* <MailIcon className="inline-block h-4 w-4 stroke-neutral-500" />{" "} */}
-              <a href="mailto:contact@chadfernandez.me">
-                contact@chadfernandez.me
-              </a>
-            </p>
-            {/* Use /assets/circuit-board.svg as background image */}
           </div>
         </div>
       </div>
