77f0c2aef77d42f025deb1e3c94c6e072cca880e
Chad Fernanadez <contact@chadfernandez.me>
2022-11-30 13:32:23 +0000

    refactored; removed deleted files, recreated to a new file

--- src/utils/TailwindUtilities.js
+++
@@ -1,3 +0,0 @@
-export function classMerge(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
--- src/utils/dateUtils.js
+++
@@ -1,8 +0,0 @@
-export const dateIsoToEnglish = (date) => {
-  const newDate = new Date(date);
-  return newDate.toLocaleString("default", {
-    month: "short",
-    day: "numeric",
-    year: "numeric",
-  });
-};
