921e70e47a54f1dfabf4c564bb079818eb5c6b97
Chad Fernanadez <contact@chadfernandez.me>
2022-12-03 14:49:03 +0000

    refactored; minified and simplified navigation. added config file for easy update

---
+++ config/items.navigation.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  items: [
+    {
+      group: "Pages",
+      items: [
+        {
+          name: "Home",
+          href: "/",
+          pin: true,
+          external: false,
+        },
+        {
+          name: "Blog",
+          href: "/blog",
+          pin: false,
+          external: false,
+        },
+        {
+          name: "Contact",
+          href: "/#contact",
+          pin: true,
+          external: false,
+        },
+      ],
+    },
+    {
+      group: "Socials",
+      items: [
+        {
+          name: "GitHub",
+          href: "https://github.com/chof64",
+          pin: false,
+          external: true,
+          icon: "/icons/socials/github.svg",
+        },
+      ],
+    },
+  ],
+};
