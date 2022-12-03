47183893c0c89434cf42e2ddacebfa881bac1681
Chad Fernanadez <contact@chadfernandez.me>
2022-12-03 12:01:03 +0000

    rmoved unnecessary lines and comments on `LayoutGlobal`

--- src/components/LayoutGlobal.js
+++ src/components/LayoutGlobal.js
@@ -1,30 +1,18 @@
-//? ./components/LayoutGlobal.js
-//* This is the global layout, which is responsible for setting the
-//* head tags of the whole application.
-//! Do not add any components to this layout, if you need to add a component,
-//! you can put it in the local layout or layout group.
-
 import Head from "next/head";
 
 export default function LayoutGlobal({ title, children }) {
-  // if title is defined, add  "defined" in the end, if not, add "undefined".
-  const titleText = title ? `${title} - defined` : `undefined - defined`;
-
   return (
     <>
       <Head>
-        {/* //? Dynamic Tags */}
         <title>
           {title
             ? "${title} - Chad Fernandez"
             : "Chad Fernandez - Student, Python, Web Developer"}
         </title>
 
-        {/* //? Static Tags */}
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        {/* //? Site favicon/icons */}
         <link rel="shortcut icon" href="/favicons/favicon.ico" />
         <link rel="manifest" href="/favicons/site.webmanifest" />
         <link
