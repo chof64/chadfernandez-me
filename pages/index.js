193f6b77ac98e8ef05a9c3130a65193944fc2c61
Chad Fernandez <contact@chadfernandez.me>
2022-12-18 17:21:19 +0000

    index page layout rework

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,42 @@
 import React from "react";
+import { ChevronDown as ChevronDownIcon } from "lucide-react";
 
 import LayoutGlobal from "/src/components/LayoutGlobal";
 import Layout from "/src/components/Layout";
-import Introduction from "/src/components/index/Introduction";
-import ContactCard from "/src/components/contact/ContactCard";
+import PlatformDefault from "/src/components/PlatformDefault";
 
 export default function index() {
   return (
     <>
-      <Introduction className="mt-16" />
-      <ContactCard className="mt-16" />
+      <PlatformDefault>
+        <div className="relative flex h-[100vh] w-full flex-col justify-center">
+          <h1 className="text-3xl font-extrabold text-neutral-800 md:text-5xl lg:text-6xl">
+            Chad Fernandez
+          </h1>
+          <div className="mt-4 max-w-md space-y-2 text-neutral-600">
+            <p>
+              I&apos;m an all around{" "}
+              <span className="font-hand text-lg font-bold text-blue-700">
+                tech savvy
+              </span>{" "}
+              earthling.
+            </p>
+            <p>
+              I&apos;m a self-taught full stack developer, working with Next.js,
+              Tailwind CSS, and Python. I like building things and sharing them
+              with the world.
+            </p>
+            <p>
+              I&apos;m from the Philippines, and currently taking Bachelor of
+              Science in Computer Science from the University of Antique.
+            </p>
+          </div>
+          <div className="absolute inset-x-0 bottom-0 flex w-full justify-center">
+            <ChevronDownIcon className="h-8 w-8 animate-bounce text-neutral-400" />
+          </div>
+        </div>
+      </PlatformDefault>
+      <div className="h-[100vh]" />
     </>
   );
 }
