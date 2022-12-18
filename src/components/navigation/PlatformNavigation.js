583ff0d90d349256079db2f6bb62a8daeaa7726f
Chad Fernandez <contact@chadfernandez.me>
2022-12-18 17:21:44 +0000

    added different platforms for different purpose

--- src/components/navigation/PlatformNavigation.js
+++ src/components/navigation/PlatformNavigation.js
@@ -4,8 +4,8 @@ import { classMerge } from "/src/utils/classMerge";
 
 export default function Platform({ children, className }) {
   return (
-    <div className={classMerge("flex h-full flex-col items-center", className)}>
-      <div className="h-full w-[95vw] md:w-[75vw] lg:w-[60vw]">{children}</div>
+    <div className={classMerge("flex h-full w-full justify-center", className)}>
+      <div className="h-full w-[95vw] md:w-[90vw] lg:w-[85vw]">{children}</div>
     </div>
   );
 }
