4f607d90752aca22a38f185037a04dc1dce88657
Chad Fernandez <contact@chadfernandez.me>
2023-01-19 14:39:13 +0800

    removed console logging when error.

--- src/lib/ghostCms/getSettings.js
+++ src/lib/ghostCms/getSettings.js
@@ -1,9 +1,6 @@
 import { authenticate } from "./authenticate";
-// import GhostContentAPI from "@tryghost/content-api";
 
 export const getSettings = async () => {
   const api = authenticate();
-  return await api.settings.browse().catch((err) => {
-    console.error(err);
-  });
+  return await api.settings.browse();
 };
