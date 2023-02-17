0889915fa91721e0999bf2d84755fe759e62329f
Chad Fernandez <contact@chadfernandez.me>
2023-02-17 19:20:28 +0800

    added open graph tags (#20)

--- src/components/layout/LayoutHead.js
+++ src/components/layout/LayoutHead.js
@@ -9,7 +9,6 @@ export default function LayoutHead({ data }) {
           ? `${data.title} - Chad Fernandez`
           : "Chad Fernandez - A web developer and a student."}
       </title>
-
       <meta
         name="description"
         content={
@@ -18,13 +17,72 @@ export default function LayoutHead({ data }) {
             : "Hello. I'm Chad, a web developer and a student from the Philippines. And this is my own little space in this vast world wide web. I write about programming and the world of technology. I also share things that I've learned along the way."
         }
       />
-
       <meta name="robots" content="index, follow" />
       <link rel="canonical" href="https://chadfernandez.me" />
-
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-
+      <meta
+        property="og:title"
+        content={
+          data?.title
+            ? `${data.title} - Chad Fernandez`
+            : "Chad Fernandez - A web developer and a student."
+        }
+      />
+      <meta property="og:url" content="https://chadfernandez.me" />
+      <meta
+        property="og:description"
+        content={
+          data?.description
+            ? data.description
+            : "Hello. I'm Chad, a web developer and a student from the Philippines. And this is my own little space in this vast world wide web. I write about programming and the world of technology. I also share things that I've learned along the way."
+        }
+      />
+      <meta property="og:site_name" content="Chad Fernandez" />
+      <meta property="og:type" content="website" />
+      <meta property="og:locale" content="en_US" />
+      <meta
+        property="og:image"
+        content="https://og.chadfernandez.me/api/chadfernandez-me"
+      />
+      <meta
+        property="og:image:secure_url"
+        content="https://og.chadfernandez.me/api/chadfernandez-me"
+      />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta property="og:image:type" content="image/png" />
+      <meta
+        property="og:image:alt"
+        content="Chad Fernandez - A web developer and a student."
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@chof64" />
+      <meta name="twitter:creator" content="@chof64" />
+      <meta
+        name="twitter:title"
+        content={
+          data?.title
+            ? `${data.title} - Chad Fernandez`
+            : "Chad Fernandez - A web developer and a student."
+        }
+      />
+      <meta
+        name="twitter:description"
+        content={
+          data?.description
+            ? data.description
+            : "Hello. I'm Chad, a web developer and a student from the Philippines. And this is my own little space in this vast world wide web. I write about programming and the world of technology. I also share things that I've learned along the way."
+        }
+      />
+      <meta
+        name="twitter:image"
+        content="https://og.chadfernandez.me/api/chadfernandez-me"
+      />
+      <meta
+        name="twitter:image:alt"
+        content="Chad Fernandez - A web developer and a student."
+      />
       <link rel="manifest" href="/favicons/site.webmanifest" />
       <link rel="shortcut icon" href="/favicons/favicon.ico" />
       <link
