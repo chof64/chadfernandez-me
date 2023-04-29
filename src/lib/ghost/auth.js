bdc06c7d04afb6080aea10e9fb5a9fa2b4cf71d4
Chad Fernandez <contact@chadfernandez.me>
2023-04-29 23:26:58 +0800

    renamed `lib/ghost`

--- src/lib/ghost/auth.js
+++ src/lib/ghost/auth.js
@@ -1,6 +1,6 @@
 import GhostContentAPI from "@tryghost/content-api";
 
-export const authenticate = () => {
+export const auth = () => {
   return new GhostContentAPI({
     url: process.env.GHOST_URL,
     key: process.env.GHOST_CONTENT_API,
