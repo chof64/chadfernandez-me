890c56e8d3e2dc31246726b40cc7a48d70a6ea57
Chad Fernandez <contact@chadfernandez.me>
2023-04-29 23:25:54 +0800

    updated `layout` components to be smaller and simpler

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,11 +1,9 @@
-import React from "react";
-
 import Platform from "@/components/Platform";
 
 export default function Footer() {
   return (
     <>
-      <Platform className="my-8" type="minimal-9">
+      <Platform className="my-8">
         <p className="text-xs text-slate-500">
           Copyright © 2023 Chad Fernandez.
         </p>
