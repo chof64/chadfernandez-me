a7755edecc34344bf25a4e3025918a32f8ca0a31
Chad Fernandez <contact@chadfernandez.me>
2023-07-06 23:37:42 +0800

    updated post page for some minor optimizations

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,7 +1,8 @@
-import React from "react"
-import Link from "next/link"
-import { readingTime } from "@tryghost/helpers"
+import React from "react";
+import Link from "next/link";
+import { readingTime } from "@tryghost/helpers";
 import { differenceInHours, format, formatDistance } from "date-fns"
+import Balancer from "react-wrap-balancer"
 
 import { auth } from "@/lib/ghost/auth"
 import Render from "@/components/render/Render"
@@ -16,14 +17,13 @@ export default async function page({ params }) {
       include: "tags",
     })
     .catch((err) => {
-      return err.response
+      // console log error type
+      console.log(err.response.status)
+      if (err.response.status === 404) {
+        return { notFound: true }
+      }
+      return { hasError: true }
     })
-
-  if (post.status) {
-    return {
-      notFound: true,
-    }
-  }
 
   const time = new Date(post.published_at)
   if (differenceInHours(new Date(), time) < 24) {
@@ -31,10 +31,6 @@ export default async function page({ params }) {
   } else {
     post.date = format(time, "MMMM do, yyyy")
   }
-  post.reading_time = readingTime(post, {
-    minute: "1 minute read",
-    minutes: "% minute read",
-  })
 
   return (
     <main>
@@ -44,7 +40,7 @@ export default async function page({ params }) {
         </Link>
         <p className="mt-16 text-sm text-muted-foreground">{post.date}</p>
         <h1 className="mt-4 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-          {post.title}
+          <Balancer>{post.title}</Balancer>
         </h1>
       </section>
       <section className="mx-auto my-32 mt-16 w-svw-95 max-w-xl">
