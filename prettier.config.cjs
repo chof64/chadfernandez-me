42c74ea16d184d4d3a933f20aebca336b1adb670
Chad Fernandez <contact@chadfernandez.me>
2023-08-27 05:06:08 +0800

    changed: minor updates on config files

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -17,6 +17,7 @@ module.exports = {
     "^@/hooks/(.*)$",
     "^@/components/ui/(.*)$",
     "^@/components/(.*)$",
+    "^@/registry/(.*)$",
     "^@/styles/(.*)$",
     "^@/app/(.*)$",
     "",
@@ -28,5 +29,5 @@ module.exports = {
   importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
   importOrderMergeDuplicateImports: true,
   importOrderCombineTypeAndValueImports: true,
-  plugins: ["@ianvs/prettier-plugin-sort-imports",'prettier-plugin-tailwindcss'],
-};
+  plugins: ["@ianvs/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
+}
