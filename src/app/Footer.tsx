613e5d4601b8c06ad76025335b89e54992d47015
Chad Fernandez <contact@chadfernandez.me>
2023-08-27 07:30:50 +0800

    hotfix: add commits not included in the last merge (#26)
    * added: link to antiquespride
    
    * changed: minor content changes on header and footer

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,12 +1,15 @@
-import React from "react"
+import React from "react";
+
+
+
+import { cn } from "@/lib/utils"
 
 export default function Footer() {
   return (
     <footer className="mx-3 my-8">
       <div className="mx-auto max-w-lg border-t pt-4">
-        <p className="muted">
-          Chad Fernandez. Made with ❤️ from the Philippines.
-        </p>
+        <p className={cn("muted mb-2")}>Site is still under development.</p>
+        <p className="muted">Made with ❤️ from the Philippines.</p>
       </div>
     </footer>
   )
