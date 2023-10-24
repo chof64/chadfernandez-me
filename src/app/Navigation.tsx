0acf3dcf0174ea91073ec955f0746177d69ad119
Chad Fernandez <contact@chadfernandez.me>
2023-10-24 13:43:23 +0800

    separated mobile and desktop navigation

--- src/app/Navigation.tsx
+++ src/app/Navigation.tsx
@@ -13,6 +13,8 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet"
 
+import MobileNav from "./MobileNav"
+
 export default function Navigation() {
   return (
     <header className="mx-3">
@@ -37,40 +39,7 @@ export default function Navigation() {
               </Link>
             ))}
           </div>
-          <div className="block sm:hidden">
-            <Sheet>
-              <SheetTrigger className="flex items-center justify-center px-0.5">
-                <div className="relative h-10 w-10 overflow-hidden">
-                  <Image
-                    src="/menu.svg"
-                    fill
-                    alt="The Burger - My hand-drawn icon"
-                  />
-                </div>
-              </SheetTrigger>
-              <SheetContent className="w-full max-w-sm">
-                <div className="relative aspect-square h-12">
-                  <Image
-                    src="/bracket.svg"
-                    fill
-                    alt="The Bracket - My hand-drawn icon"
-                  />
-                </div>
-
-                <div className="mt-8 flex flex-col gap-4">
-                  {main.map((item) => (
-                    <Link
-                      className="text-xl text-neutral-600 hover:text-black"
-                      href={item.href}
-                      key={item.name}
-                    >
-                      {item.name}
-                    </Link>
-                  ))}
-                </div>
-              </SheetContent>
-            </Sheet>
-          </div>
+          <MobileNav />
         </nav>
       </div>
     </header>
