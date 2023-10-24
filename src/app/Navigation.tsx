9819c23dd83a979ffb4f11810cd176e425720e57
Chad Fernandez <contact@chadfernandez.me>
2023-10-24 13:46:24 +0800

    Fix mobile navigation menu behavior on click (#32)
    * separated mobile and desktop navigation
    
    * added state to track if menu tray is open or not
    
    * clean up unused imports on navigation bar

--- src/app/Navigation.tsx
+++ src/app/Navigation.tsx
@@ -3,15 +3,7 @@ import Image from "next/image"
 import Link from "next/link"
 import { main } from "@/data/navigation/main"
 
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet"
+import MobileNav from "./MobileNav"
 
 export default function Navigation() {
   return (
@@ -37,40 +29,7 @@ export default function Navigation() {
               </Link>
             ))}
           </div>
-          <div className="block sm:hidden">
-            <Sheet>
-              <SheetTrigger className="flex items-center justify-center px-0.5">
-                <div className="relative h-10 w-10 overflow-hidden">
-                  <Image
-                    src="/menu.svg"
-                    fill
-                    alt="The Burger - My hand-drawn icon"
-                  />
-                </div>
-              </SheetTrigger>
-              <SheetContent className="w-full max-w-sm">
-                <div className="relative aspect-square h-12">
-                  <Image
-                    src="/bracket.svg"
-                    fill
-                    alt="The Bracket - My hand-drawn icon"
-                  />
-                </div>
-
-                <div className="mt-8 flex flex-col gap-4">
-                  {main.map((item) => (
-                    <Link
-                      className="text-xl text-neutral-600 hover:text-black"
-                      href={item.href}
-                      key={item.name}
-                    >
-                      {item.name}
-                    </Link>
-                  ))}
-                </div>
-              </SheetContent>
-            </Sheet>
-          </div>
+          <MobileNav />
         </nav>
       </div>
     </header>
