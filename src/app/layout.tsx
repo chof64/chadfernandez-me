4c600b566bc0ed6ac4c3591e74d7d5e99a27927e
Chad Fernandez <contact@chadfernandez.me>
2023-10-24 09:42:31 +0800

    added google analytics tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,12 @@
+import "./globals.css"
 
+import type { Metadata } from "next"
+import { Inter, JetBrains_Mono } from "next/font/google"
+import { Analytics } from "@vercel/analytics/react"
+import { GoogleAnalytics } from "nextjs-google-analytics"
 
-
-import "./globals.css";
-
-
-
-import type { Metadata } from "next";
-import { Inter, JetBrains_Mono } from "next/font/google";
-import { Analytics } from "@vercel/analytics/react";
-
-
-
-import Footer from "./Footer";
-import Navigation from "./Navigation";
-
+import Footer from "./Footer"
+import Navigation from "./Navigation"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -64,6 +57,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         </div>
         <Footer />
         <Analytics />
+        <GoogleAnalytics trackPageViews />
       </body>
     </html>
   )
