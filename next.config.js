c0ee93f28dbeaf64cf3e223a23c4f4a2e18b0c76
Chad Fernandez <contact@chadfernandez.me>
2023-12-11 13:50:11 +0800

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: [
-      "res.cloudinary.com",
-      "prod-files-secure.s3.us-west-2.amazonaws.com",
-    ],
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,113 @@
-import React from "react"
-import Image from "next/image"
-import Link from "next/link"
-
-import { cn } from "@/lib/utils"
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="mx-4">
-      <section className="mx-auto my-8 mt-16 max-w-lg">
-        <h1 className="h1">hey, I&apos;m Chad👋</h1>
-        <p className="p">
-          I&apos;m a multidisciplinary enthusiast, straddling the worlds of web
-          development, design, and academia. Currently pursuing my Computer
-          Science degree at the{" "}
-          <Link
-            className="link"
-            href="https://www.antiquespride.edu.ph/"
-            target="_black"
-            rel="noreferrer noopener nofollow noreferrer"
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold">src/app/page.tsx</code>
+        </p>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            University of Antique
-          </Link>
-          , I channel my spare time into crafting digital experiences.
-        </p>
-      </section>
+            By{' '}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
 
-      <section className="mx-auto my-8 max-w-lg">
-        <div className="relative h-52 rounded-lg shadow md:h-64 transition delay-150 duration-100 ease-in hover:rotate-2">
-          <Image
-            className="rounded-lg bg-gradient-to-br from-sky-200 to-purple-200 object-cover object-center"
-            src="https://res.cloudinary.com/dmk5o9frj/image/upload/f_auto,q_auto/v1693036339/chadfernandez.me/Home/hack4gov-group_dgfd0w.jpg"
-            fill
-            alt="HackForGov 2 Group Photo"
-          />
-        </div>
-        <div className="mt-3 columns-2 gap-3">
-          <div className="relative h-52 rounded-lg shadow md:h-64 transition delay-150 duration-100 ease-in hover:-rotate-2">
-            <Image
-              className="rounded-lg bg-gradient-to-br from-sky-200 to-purple-200 object-cover object-bottom"
-              src="https://res.cloudinary.com/dmk5o9frj/image/upload/f_auto,q_auto/v1693036340/chadfernandez.me/Home/hack4gov-table_du5e93.jpg"
-              fill
-              alt="HackForGov 2 Photo"
-            />
-          </div>
-          <div className="relative h-52 rounded-lg shadow md:h-64 transition delay-150 duration-100 ease-in hover:rotate-2">
-            <Image
-              className="rounded-lg bg-gradient-to-br from-sky-200 to-purple-200 object-cover object-center"
-              src="https://res.cloudinary.com/dmk5o9frj/image/upload/f_auto,q_auto/v1693036336/chadfernandez.me/Home/hack4gov-solo_vhsxsy.jpg"
-              fill
-              alt="HackForGov 2 Solo Photo"
-            />
-          </div>
-        </div>
-        <div className="mt-2">
-          <p className={cn("muted text-center text-xs")}>
-            HackForGov 2 Competition by DICT Region VI (Aug 18, 2023).
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Docs{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Find in-depth information about Next.js features and API.
           </p>
-        </div>
-      </section>
+        </a>
 
-      <section className="mx-auto my-8 max-w-lg">
-        <p className="p">
-          I&apos;m fascinated by all things tech! I enjoy diving into the world
-          of technology, whether it&apos;s building cool websites or managing
-          service deployments with Docker. I&apos;m a fan of using JavaScript,
-          especially Next.js, and I&apos;m drawn to a clean and minimal design
-          style with the help of Tailwind CSS.
-        </p>
-      </section>
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Learn{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          </p>
+        </a>
 
-      {/* INSERT SOME CASE STUDIES HERE, AT MOST 2 ITEMS */}
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Templates{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Explore starter templates for Next.js.
+          </p>
+        </a>
 
-      <section className="mx-auto my-8 max-w-lg">
-        <p className="p">
-          I have this urge to learn more and believe in learning by doing. So, I
-          like to get my hands dirty and experiment with things. This hands-on
-          approach helps me grasp concepts quicker and keeps me on a path of
-          constant improvement. And I&apos;ve developed this platform—to share
-          my journey and the exciting things I&apos;ve discover.
-        </p>
-      </section>
-
-      {/* INSERT SOME BLOG POST HERE, AT MOST 3 ITEMS */}
-
-      <section className="mx-auto my-8 mb-2 max-w-lg">
-        <p className="p">
-          If you're like me and enjoy exploring the fascinating world of
-          technology, let's connect. Feel free to reach out - I'm always up for
-          a chat!
-        </p>
-        <p className="p">Let&apos;s build, design, and learn together.</p>
-      </section>
-      <section className="mx-auto my-8 mb-16 mt-2 max-w-lg">
-        <div className="relative aspect-video h-12">
-          <Image className="" src="/chad.svg" fill alt="Handwriting" />
-        </div>
-      </section>
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Deploy{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
     </main>
   )
 }
