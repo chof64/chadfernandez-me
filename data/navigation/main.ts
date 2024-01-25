69e66466f3a79799f268bd7b2165636a76f187c4
Chad Fernandez <developer@chadfernandez.me>
2024-01-26 00:41:57 +0800

    initial commit with t3 app boilerplate

--- data/navigation/main.ts
+++
@@ -1,9 +0,0 @@
-type main = {
-  name: string
-  href: string
-}
-
-export const main: main[] = [
-  { name: "home", href: "/" },
-  { name: "blog", href: "/blog" },
-]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,67 @@
-import React from "react"
-import Image from "next/image"
-import Link from "next/link"
+import { unstable_noStore as noStore } from "next/cache";
+import Link from "next/link";
 
-import { cn } from "@/lib/utils"
+import { CreatePost } from "~/app/_components/create-post";
+import { api } from "~/trpc/server";
 
-export default function Home() {
+export default async function Home() {
+  noStore();
+  const hello = await api.post.hello.query({ text: "from tRPC" });
+
   return (
-    <main className="mx-4">
-      <section className="mx-auto my-8 mt-16 max-w-lg">
-        <h1 className="h1">hey, I&apos;m Chad👋</h1>
-        <p className="p">
-          I&apos;m a multidisciplinary enthusiast, straddling the worlds of web
-          development, design, and academia. Currently pursuing my Computer
-          Science degree at the{" "}
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+        </h1>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <Link
-            className="link"
-            href="https://www.antiquespride.edu.ph/"
-            target="_black"
-            rel="noreferrer noopener nofollow noreferrer"
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
+            href="https://create.t3.gg/en/usage/first-steps"
+            target="_blank"
           >
-            University of Antique
+            <h3 className="text-2xl font-bold">First Steps →</h3>
+            <div className="text-lg">
+              Just the basics - Everything you need to know to set up your
+              database and authentication.
+            </div>
           </Link>
-          , I channel my spare time into crafting digital experiences.
-        </p>
-      </section>
-
-      <section className="mx-auto my-8 max-w-lg">
-        <div className="relative h-52 rounded-lg shadow md:h-64 transition delay-150 duration-100 ease-in hover:rotate-2">
-          <Image
-            className="rounded-lg bg-gradient-to-br from-sky-200 to-purple-200 object-cover object-center"
-            src="https://res.cloudinary.com/dmk5o9frj/image/upload/f_auto,q_auto/v1693036339/chadfernandez.me/Home/hack4gov-group_dgfd0w.jpg"
-            fill
-            alt="HackForGov 2 Group Photo"
-          />
+          <Link
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
+            href="https://create.t3.gg/en/introduction"
+            target="_blank"
+          >
+            <h3 className="text-2xl font-bold">Documentation →</h3>
+            <div className="text-lg">
+              Learn more about Create T3 App, the libraries it uses, and how to
+              deploy it.
+            </div>
+          </Link>
         </div>
-        <div className="mt-3 columns-2 gap-3">
-          <div className="relative h-52 rounded-lg shadow md:h-64 transition delay-150 duration-100 ease-in hover:-rotate-2">
-            <Image
-              className="rounded-lg bg-gradient-to-br from-sky-200 to-purple-200 object-cover object-bottom"
-              src="https://res.cloudinary.com/dmk5o9frj/image/upload/f_auto,q_auto/v1693036340/chadfernandez.me/Home/hack4gov-table_du5e93.jpg"
-              fill
-              alt="HackForGov 2 Photo"
-            />
-          </div>
-          <div className="relative h-52 rounded-lg shadow md:h-64 transition delay-150 duration-100 ease-in hover:rotate-2">
-            <Image
-              className="rounded-lg bg-gradient-to-br from-sky-200 to-purple-200 object-cover object-center"
-              src="https://res.cloudinary.com/dmk5o9frj/image/upload/f_auto,q_auto/v1693036336/chadfernandez.me/Home/hack4gov-solo_vhsxsy.jpg"
-              fill
-              alt="HackForGov 2 Solo Photo"
-            />
-          </div>
-        </div>
-        <div className="mt-2">
-          <p className={cn("muted text-center text-xs")}>
-            HackForGov 2 Competition by DICT Region VI (Aug 18, 2023).
+        <div className="flex flex-col items-center gap-2">
+          <p className="text-2xl text-white">
+            {hello ? hello.greeting : "Loading tRPC query..."}
           </p>
         </div>
-      </section>
 
-      <section className="mx-auto my-8 max-w-lg">
-        <p className="p">
-          I&apos;m fascinated by all things tech! I enjoy diving into the world
-          of technology, whether it&apos;s building cool websites or managing
-          service deployments with Docker. I&apos;m a fan of using JavaScript,
-          especially Next.js, and I&apos;m drawn to a clean and minimal design
-          style with the help of Tailwind CSS.
-        </p>
-      </section>
-
-      {/* INSERT SOME CASE STUDIES HERE, AT MOST 2 ITEMS */}
-
-      <section className="mx-auto my-8 max-w-lg">
-        <p className="p">
-          I have this urge to learn more and believe in learning by doing. So, I
-          like to get my hands dirty and experiment with things. This hands-on
-          approach helps me grasp concepts quicker and keeps me on a path of
-          constant improvement. And I&apos;ve developed this platform—to share
-          my journey and the exciting things I&apos;ve discover.
-        </p>
-      </section>
-
-      {/* INSERT SOME BLOG POST HERE, AT MOST 3 ITEMS */}
-
-      <section className="mx-auto my-8 mb-2 max-w-lg">
-        <p className="p">
-          If you're like me and enjoy exploring the fascinating world of
-          technology, let's connect. Feel free to reach out - I'm always up for
-          a chat!
-        </p>
-        <p className="p">Let&apos;s build, design, and learn together.</p>
-      </section>
-      <section className="mx-auto my-8 mb-16 mt-2 max-w-lg">
-        <div className="relative aspect-video h-12">
-          <Image className="" src="/chad.svg" fill alt="Handwriting" />
-        </div>
-      </section>
+        <CrudShowcase />
+      </div>
     </main>
-  )
+  );
+}
+
+async function CrudShowcase() {
+  const latestPost = await api.post.getLatest.query();
+
+  return (
+    <div className="w-full max-w-xs">
+      {latestPost ? (
+        <p className="truncate">Your most recent post: {latestPost.name}</p>
+      ) : (
+        <p>You have no posts yet.</p>
+      )}
+
+      <CreatePost />
+    </div>
+  );
 }
