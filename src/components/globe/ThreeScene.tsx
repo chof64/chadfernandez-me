6205d6a8a824f8da348db49d18c4cd90ec152635
Chad Fernandez <developer@chadfernandez.me>
2024-01-27 19:15:15 +0800

    feat: add globe components and shadcn button with deps
    This adds the globe component and its deps with threejs and r3f. Also added shadcn-ui button
    components and deps

---
+++ src/components/globe/ThreeScene.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import ThreeMesh from "./ThreeMesh";
+export default function ThreeScene() {
+  return (
+    <Canvas
+      camera={{
+        fov: 75,
+        position: [0, 0, 2.1],
+      }}
+      gl={{ antialias: true }}
+    >
+      <OrbitControls
+        enableRotate={false}
+        enableZoom={false}
+        enablePan={false}
+        autoRotate
+        autoRotateSpeed={1}
+      />
+      <ambientLight intensity={1.3} />
+      <pointLight position={[-10, -10, -10]} intensity={0.4} />
+      <ThreeMesh />
+    </Canvas>
+  );
+}
