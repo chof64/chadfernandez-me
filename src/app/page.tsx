9193b4575b6a675b343022556e140e189bd49d2d
Chad Fernandez <developer@chadfernandez.me>
2024-02-18 14:55:47 +0800

    feat: redesigned the whole of home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import ThreeScene from "~/components/globe/ThreeScene";
 import { Button } from "~/components/ui/button";
 
@@ -5,32 +6,83 @@ export default function Home() {
   return (
     <main>
       <section className="relative">
-        <div className="container pt-[32rem] lg:flex">
-          <div className="lg:w-3/5">
-            <h2 className="text-3xl font-bold tracking-tight opacity-80 transition-all duration-500 ease-in-out sm:text-5xl lg:text-6xl">
-              Chad Fernandez.
-            </h2>
-            <h1 className="text-3xl font-bold tracking-tight opacity-80 transition-all duration-500 ease-in-out sm:text-5xl lg:text-6xl">
-              Web Developer.
-            </h1>
+        <div className="container max-w-2xl pt-32">
+          <div>
+            <h1 className="font-semibold leading-tight">Chad Fernandez</h1>
+            <h2 className="leading-tight opacity-60">Web Developer</h2>
           </div>
-          <div className="lg:w-2/5">
-            <p className="mt-4 text-2xl font-medium leading-7 opacity-80 transition-all duration-500 ease-in-out lg:mt-0">
-              <span className="text-neutral-500 dark:text-neutral-400">
-                I&apos;m a multidisciplinary enthusiast, straddling the worlds
-                of web development, design, and academia.
-              </span>{" "}
-              Crafting digital experiences that have a positive impact is my
-              passion.
+          <div className="mt-6">
+            <p className="leading-7 [&:not(:first-child)]:mt-6">
+              I&apos;m a multidisciplinary enthusiast, straddling the worlds of
+              web development, design, and academia. Currently pursuing my
+              Information Technology degree at the University of Antique, I
+              channel my spare time into crafting digital experiences.
             </p>
-            <div className="mt-6 flex gap-4">
-              <Button>GitHub</Button>
-              <Button>contact@chadfernandez.me</Button>
-            </div>
+          </div>
+          <div className="mt-6 flex flex-wrap gap-2">
+            <Button
+              className="h-fit rounded-md border border-white/10 bg-white/10 px-2 py-0.5 text-white backdrop-blur-sm hover:border-white/20 hover:bg-white/20 "
+              asChild
+            >
+              <Link
+                href="https://github.com/chof64"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                github.com/chof64
+              </Link>
+            </Button>
+            <Button
+              className="h-fit rounded-md border border-white/10 bg-white/10 px-2 py-0.5 text-white backdrop-blur-sm hover:border-white/20 hover:bg-white/20"
+              asChild
+            >
+              <Link
+                href="mailto:contact@chadfernandez.me"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                mailto:contact@chadfernandez.me
+              </Link>
+            </Button>
           </div>
         </div>
-        <div className="absolute inset-x-0 top-0 -z-10 h-[64rem] w-full">
+        <div className="py-56" />
+        <div className="absolute inset-x-0 top-64 -z-50 h-[52rem] w-full">
           <ThreeScene />
+        </div>
+      </section>
+
+      <section>
+        <div className="container max-w-2xl">
+          <p className="leading-7 [&:not(:first-child)]:mt-6">
+            I&apos;m fascinated by all things tech! I enjoy diving into the
+            world of technology, whether it&apos;s building cool websites or
+            managing service deployments with Docker. I&apos;m a fan of using
+            JavaScript, especially Next.js, and I&apos;m drawn to a clean and
+            minimal design style with the help of Tailwind CSS.
+          </p>
+        </div>
+      </section>
+
+      <section className="mt-6">
+        <div className="container max-w-2xl">
+          <p className="leading-7 [&:not(:first-child)]:mt-6">
+            I have this urge to learn more and believe in learning by doing. So,
+            I like to get my hands dirty and experiment with things. This
+            hands-on approach helps me grasp concepts quicker and keeps me on a
+            path of constant improvement. And I&apos;ve developed this
+            platform—to share my journey and the exciting things I&apos;ve
+            discover.
+          </p>
+        </div>
+      </section>
+      <section className="mb-32 mt-6">
+        <div className="container max-w-2xl">
+          <p className="leading-7 [&:not(:first-child)]:mt-6">
+            If you&apos;re like me and enjoy exploring the fascinating world of
+            technology, let&apos;s connect. Feel free to reach out - I&apos;m
+            always up for a chat! Let&apos;s build, design, and learn together.
+          </p>
         </div>
       </section>
     </main>
