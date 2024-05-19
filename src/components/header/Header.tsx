3a62b24a0323941d1bc07bc6b669d87299460384
Chad Fernandez <developer@chadfernandez.me>
2024-05-19 17:41:45 +0800

    feat(comp/header): added minimum header elements to be expanded later

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="container sticky top-0 flex h-16 items-center">
+      <section>
+        <div className="leading-none">
+          <div className="font-medium">Chad Fernandez</div>
+        </div>
+      </section>
+    </header>
+  );
+}
