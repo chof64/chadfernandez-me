89dbc60e9667e2eba550aff3e795d07f2ef53a73
Chad Fernandez <developer@chadfernandez.me>
2024-05-30 23:28:45 +0800

    feat: updated root layout metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "~/styles/globals.css";
 import { GeistSans } from "geist/font/sans";
 import { Caveat } from "next/font/google";
 
+import { env } from "~/env";
 import { cn } from "~/lib/utils";
 import { TRPCReactProvider } from "~/trpc/react";
 
@@ -16,9 +17,36 @@ const caveat = Caveat({
 });
 
 export const metadata = {
-  title: "Chad Fernandez",
+  title: { template: "%s", default: "Chad Fernandez" },
   description: "A student and a web developer from the Philippines.",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
+  metadataBase: new URL(env.BASE_URL || "https://www.chadfernandez.me"),
+  openGraph: {
+    siteName: "Chad Fernandez",
+    url: env.BASE_URL || "https://www.chadfernandez.me",
+    locale: "en_US",
+    type: "website",
+    images: [
+      {
+        url: "/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Chad Fernandez",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Chad Fernandez",
+    images: [
+      {
+        url: "/twitter-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Chad Fernandez",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
--- src/env.js
+++ src/env.js
@@ -8,6 +8,7 @@ export const env = createEnv({
    */
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
+    BASE_URL: z.string().url(),
   },
 
   /**
@@ -25,6 +26,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
+    BASE_URL: process.env.BASE_URL,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
