02a92f8c1004fdc4cfe19110fc76761adb1dc8fc
Chad Fernandez <developer@chadfernandez.me>
2024-06-22 10:14:12 +0800

    refactor: minor config updates and header updates

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,30 +3,32 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="container sticky top-0 flex h-16 max-w-3xl items-center justify-between px-6 sm:px-8">
-      <section>
-        <div className="leading-none">
-          <Link href="/">
-            <div className="font-semibold transition-colors delay-75 duration-300 ease-in-out hover:text-cyan-700">
-              Chad Fernandez
-            </div>
+    <header className="sticky top-0 z-50">
+      <div className="container flex h-12 max-w-2xl items-center justify-between bg-white/80 px-6 backdrop-blur sm:px-8">
+        <section>
+          <div className="leading-none">
+            <Link href="/">
+              <div className="font-semibold transition-colors delay-75 duration-300 ease-in-out hover:text-cyan-700">
+                Chad Fernandez
+              </div>
+            </Link>
+          </div>
+        </section>
+        <section className="flex gap-4 hover:text-neutral-500">
+          <Link
+            className="font-medium transition-colors delay-75 duration-300 ease-in-out hover:text-cyan-700"
+            href="/"
+          >
+            Home
           </Link>
-        </div>
-      </section>
-      <section className="flex gap-4 hover:text-neutral-500">
-        <Link
-          className="font-medium transition-colors delay-75 duration-300 ease-in-out hover:text-cyan-700"
-          href="/"
-        >
-          Home
-        </Link>
-        <Link
-          className="font-medium transition-colors delay-75 duration-300 ease-in-out hover:text-cyan-700"
-          href="/post"
-        >
-          Blog
-        </Link>
-      </section>
+          <Link
+            className="font-medium transition-colors delay-75 duration-300 ease-in-out hover:text-cyan-700"
+            href="/post"
+          >
+            Blog
+          </Link>
+        </section>
+      </div>
     </header>
   );
 }
