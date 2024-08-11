81d818871f4021df2e9e630fa1a810053b7e7a8e
Chad Fernandez <developer@chadfernandez.me>
2024-08-11 12:36:28 +0800

    refactor: remove ghost cms dependencies and config

--- src/env.js
+++ src/env.js
@@ -8,9 +8,6 @@ export const env = createEnv({
    */
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
-
-    GHOST_API_URL: z.string().url(),
-    GHOST_CONTENT_API_KEY: z.string(),
   },
 
   /**
@@ -29,9 +26,6 @@ export const env = createEnv({
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
-
-    GHOST_API_URL: process.env.GHOST_API_URL,
-    GHOST_CONTENT_API_KEY: process.env.GHOST_CONTENT_API_KEY,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
