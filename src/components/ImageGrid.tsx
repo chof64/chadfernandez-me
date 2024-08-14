ad1d6daf1ce5e0317c384ed0b96218105de31349
Chad Fernandez <developer@chadfernandez.me>
2024-08-14 13:58:36 +0800

    feat: add new homepage content and image assets

--- src/components/ImageGrid.tsx
+++ src/components/ImageGrid.tsx
@@ -1,47 +1,53 @@
 import React from "react";
 import Image from "next/image";
+import { cn } from "~/lib/utils";
 
-export default function ImageGrid() {
+export default function ImageGrid({ className }: { className?: string }) {
   return (
-    <div className="my-8 grid h-[30rem] grid-cols-3 grid-rows-3 gap-2 brightness-90">
+    <div
+      className={cn(
+        "my-8 grid h-[36rem] grid-cols-2 grid-rows-4 gap-2 sm:h-[30rem] sm:grid-cols-3 sm:grid-rows-3",
+        className,
+      )}
+    >
       <div className="relative col-span-1 row-span-2 aspect-auto h-full w-full">
         <Image
           className="rounded-xl object-cover object-center"
-          src="/imagegrid/hack4govpoints.jpg"
+          src="/imagegrid/hack4gov3-travel.jpeg"
           fill
-          alt="Hack4Gov"
+          alt=""
         />
       </div>
       <div className="relative col-span-1 row-span-1 w-full">
         <Image
           className="rounded-xl object-cover object-center"
-          src="/imagegrid/hack4govteam.jpg"
+          src="/imagegrid/hack4gov3-group.jpeg"
           fill
-          alt="Hack4Gov"
+          alt=""
         />
       </div>
-      <div className="relative col-span-1 row-span-3 aspect-auto h-full w-full">
+      <div className="relative col-span-1 row-span-2 aspect-auto h-full w-full sm:row-span-3">
         <Image
           className="rounded-xl object-cover object-center"
-          src="/imagegrid/nstwsolo.jpg"
+          src="/imagegrid/hack4gov3-card.jpeg"
           fill
-          alt="NSTW"
+          alt=""
         />
       </div>
       <div className="relative w-full">
         <Image
           className="rounded-xl object-cover object-center"
-          src="/imagegrid/hack4govteam2.jpg"
+          src="/imagegrid/hack4gov3-team.jpeg"
           fill
-          alt="Hack4Gov"
+          alt=""
         />
       </div>
       <div className="relative col-span-2 row-span-1 w-full">
         <Image
           className="rounded-xl object-cover object-center"
-          src="/imagegrid/nstwteam.jpg"
+          src="/imagegrid/hack4gov3-solo.jpeg"
           fill
-          alt="NSTW"
+          alt=""
         />
       </div>
     </div>
---
+++ src/components/ParticipationShowcase.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { cn } from "~/lib/utils";
+import Heading from "./white-design/Heading";
+
+export default function ParticipationShowcase({
+  className,
+}: {
+  className?: string;
+}) {
+  const data = [
+    {
+      heading: "Regional HackForGov 3 Competition",
+      subheading: "Jul 2024 | Iloilo City, Philippines",
+      description: (
+        <>
+          A nation wide cybersecurity competition in a Capture-the-Flag (CTF)
+          format organized by the Deparment of Information and Communications
+          Technology (DICT).
+        </>
+      ),
+      highlights: [
+        "Wildcard Participant to the National HackForGov 3 Competition.",
+        "Excellence Awardee for the Most Individual Points.",
+        "1st Runner up on the team category.",
+      ],
+    },
+    {
+      heading: "National Science and Technology Week (NSTW) 2023",
+      subheading: "Nov 2023 | Iloilo City, Philippines",
+      description: (
+        <>
+          An event hosted by the Department of Science and Technology (DOST)
+          that highlights the significant contributions of science and
+          technology to national development.
+        </>
+      ),
+      highlights: [
+        "Attended the Regional Basic Caravan in Region VI Seminar.",
+        "Attended the NSTW Startup Jam Pitching competition.",
+      ],
+    },
+    {
+      heading: "Regional HackForGov 2 Competition",
+      subheading: "Aug 2023 | Iloilo City, Philippines",
+      description: (
+        <>
+          A nation wide cybersecurity competition in a Capture-the-Flag (CTF)
+          format organized by the Deparment of Information and Communications
+          Technology (DICT).
+        </>
+      ),
+      highlights: [
+        "Secured 6th place in the individual rankings.",
+        "5th Place on the team rankings.",
+        "First time participant in the competition.",
+      ],
+    },
+  ] as const;
+
+  return (
+    <div className={cn("space-y-12", className)}>
+      {data.map((item) => (
+        <div className="group" key={item.heading}>
+          <Heading
+            className="transition-color delay-75 duration-150 ease-in-out group-hover:text-accent-foreground"
+            order={3}
+            style={4}
+          >
+            {item.heading}
+          </Heading>
+          <p className="typography-muted">{item.subheading}</p>
+          <p className="typography-p !mt-3">{item.description}</p>
+          <ul className="my-4 ml-6 list-disc [&>li]:mt-1.5">
+            {item.highlights.map((highlight) => (
+              <li key={highlight}>{highlight}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+}
