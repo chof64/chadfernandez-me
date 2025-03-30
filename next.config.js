d0eda3efe7b0df1ba3904958c9aa872456ec156b
Chad Fernandez <developer@chadfernandez.me>
2025-03-30 18:19:03 +0800

    add hashnode fetcher and image hostname

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.hashnode.com",
+      },
+    ],
+  },
+};
 
 export default config;
