2b5576c0a093f8bdac7a93452f55f8130df78d96
Chad Fernandez <developer@chadfernandez.me>
2025-07-16 17:12:22 +0800

    refactor: design changes and refined content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,64 +1,58 @@
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from '~/components/ui/button';
+import HomeEngagements from './home-engagements';
 
-import HomeEngagements from "./HomeEngagements";
-
-export default function PageIndex() {
+export default function IndexPage() {
   return (
-    <div className="container my-16 max-w-2xl">
-      <section className="mt-40 w-full">
+    <div className="container my-16 max-w-xl">
+      <section className="mt-32">
         <Image
-          className="h-60 w-48 rounded-xl object-cover"
-          src="/hero-portrait.jpeg"
-          width={192}
-          height={240}
           alt="Portrait"
+          className="h-24 w-24 rounded-xl object-cover"
+          height={96}
           priority
+          src="/hero-portrait.jpeg"
+          width={96}
         />
-        <h1 className="mt-8 font-semibold text-2xl">
-          I&apos;m Chad Fernandez &mdash; web developer
+        <h1 className="mt-6 font-semibold text-xl tracking-wide">
+          👋 Hello, I&apos;m Chad Fernandez
         </h1>
-        <p className="mt-2 text-balance font-semibold text-2xl text-muted-foreground/60">
-          building web applications that make a difference in the community
+        <p className="mt-6 font-serif leading-7">
+          I am a software engineer and a student studying BS Information
+          Technology.
         </p>
-      </section>
-      <section className="mt-24">
-        <h2 className="font-semibold text-sm tracking-tight">About Me</h2>
-        <p className="mt-6 leading-7">
-          Driven by a love for technology and a mission to build web apps that
-          matter. My passion for coding drives me to create functional,
-          minimalistic websites that solve real-world problems.
-        </p>
-        <p className="mt-6 leading-7">
-          I&apos;m a student studying Information Technology and on a journey of
-          learning and growing as a web developer.
+        <p className="mt-6 font-serif leading-7">
+          Driven by the love for technology and a mission to build apps that
+          make a difference in the community. My passion for coding drives me to
+          create functional, minimalistic apps that solve real-world problems.
         </p>
       </section>
       <HomeEngagements />
-      <section className="mt-24">
-        <h2 className="font-semibold text-sm tracking-tight">Contacts</h2>
-        <p className="mt-6 leading-7">
-          If you&apos;re like me and enjoy exploring the fascinating world of
-          technology, let&apos;s connect. Feel free to reach out - I&apos;m
-          always up for a chat! Let&apos;s build, design, and learn together.
+      <section className="mt-16">
+        <h2 className="font-semibold text-lg">Contacts</h2>
+        <p className="mt-6 font-serif leading-7">
+          If you're like me and enjoy exploring the fascinating world of
+          technology, let's connect. Feel free to reach out - I'm always up for
+          a chat! Let's build, design, and learn together.
         </p>
-        <p className="mt-6">
-          <Link
-            className="font-medium text-muted-foreground underline transition-colors duration-300 ease-in-out hover:text-primary"
-            href="mailto:contact@chadfernandez.me"
-          >
-            contact@chadfernandez.me
-          </Link>
-        </p>
-        <p>
-          <Link
-            className="font-medium text-muted-foreground underline transition-colors duration-300 ease-in-out hover:text-primary"
-            href="https://github.com/chof64"
-          >
-            github.com/chof64
-          </Link>
-        </p>
+        <div className="mt-6 flex flex-col items-start gap-3">
+          <Button variant={'secondary'}>
+            <Link href="mailto:contact@chadfernandez.me" target="_blank">
+              contact@chadfernandez.me
+            </Link>
+          </Button>
+          <Button variant={'secondary'}>
+            <Link href="https://github.com/chof64" target="_blank">
+              github.com/chof64
+            </Link>
+          </Button>
+          <Button variant={'secondary'}>
+            <Link href="https://www.linkedin.com/in/chof64" target="_blank">
+              linkedin.com/in/chof64
+            </Link>
+          </Button>
+        </div>
       </section>
     </div>
   );
