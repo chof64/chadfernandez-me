2213338281b183f89e78dffd42498bd9fc5f1fc6
Chad Fernandez <developer@chadfernandez.me>
2025-12-07 10:59:36 +0800

    feat: update site configuration and add dotenv and sharp dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,12 @@
+import "dotenv/config";
+
 import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-import { loadEnv } from "vite";
-
-const { PUBLIC_BASE_URL } = loadEnv(
-  process.env.NODE_ENV || "development",
-  process.cwd(),
-  ""
-);
 
 export default defineConfig({
-  site: PUBLIC_BASE_URL,
+  site: process.env.BASE_URL,
   vite: {
     plugins: [tailwindcss()],
   },
