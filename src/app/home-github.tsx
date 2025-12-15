5cdc4fd5d654dde75cfd6749af1f9842ac673806
Chad Fernandez <developer@chadfernandez.me>
2025-12-16 02:33:26 +0800

    feat: add GitHub calendar component to the homepage

---
+++ src/app/home-github.tsx
@@ -0,0 +1,11 @@
+import { GitHubCalendar } from "react-github-calendar";
+
+export default function HomeGitHub() {
+  return (
+    <GitHubCalendar
+      colorScheme="light"
+      showColorLegend={false}
+      username="chof64"
+    />
+  );
+}
